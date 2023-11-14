6df31b902470f1c86b0dc641eaf062be220135d2
tttatata <85152122+tttatata@users.noreply.github.com>
2023-11-14 19:54:54 +0700

    âscc

--- admin/components/Nav.js
+++ admin/components/Nav.js
@@ -26,7 +26,7 @@ export default function Nav({ show }) {
     <aside
       className={
         (show ? "left-0" : "-left-full") +
-        " top-0 text-gray-500 p-4 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"
+        " top-0 text-gray-500 p-4 fixed w-full bg-white h-full md:static md:w-auto transition-all"
       }
     >
       <Link
